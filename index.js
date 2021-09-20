381f5c7184f42cdf240271572c378d562e897ab9
Aidan Margo <aidanmargo@gmail.com>
2021-09-20 11:18:52 -0500

    commit tree 4db832b0bd30ce028fac74cce00240a074e72376

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
+const num1 = 31;
+const num2 = 2;
+const num3 = 26;
+const num4 = 11;
+const multiply = num1*num2;
+const mod = num3%num4;
+const random = Math.floor(Math.random())+ 1;
+const max = Math.max(0, 1, 5, 10, 20);
 // Write your code here
